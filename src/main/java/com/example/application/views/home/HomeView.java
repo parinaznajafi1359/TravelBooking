328aaec37850776925207ff22d1891687455a639
parinaznajafi <parinaz.najafi1359@gmail.com>
2026-06-21 19:30:08 +0200

    Component

--- src/main/java/com/example/application/views/home/HomeView.java
+++ src/main/java/com/example/application/views/home/HomeView.java
@@ -8,7 +8,11 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import org.vaadin.lineawesome.LineAwesomeIconUrl;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
 
 @PageTitle("Home")
 @Route("Home")
@@ -19,17 +23,40 @@ public class HomeView extends HorizontalLayout {
     private Button sayHello;
 
     public HomeView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
+        setSpacing(false);
+        setAlignItems(Alignment.CENTER);
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        H1 companyName = new H1("TravelBooking");
+        companyName.getStyle()
+                .set("font-family", "cursive")
+                .set("font-size", "6rem")
+                .set("margin", "0");
 
-        add(name, sayHello);
+        H2 subName = new H2("... your travel booking place ...");
+        subName.getStyle()
+                .set("margin", "0")
+                .set("color", "gray");
+
+        Image img = new Image("images/logo.png", "TravelBooking Logo");
+        img.setWidth("220px");
+
+        Paragraph description = new Paragraph(
+                "Bei TravelBooking können Kunden einfache und moderne Reisen buchen. " +
+                        "Die Anwendung zeigt verschiedene Angebote, Preise und Informationen " +
+                        "übersichtlich an. Ziel ist es, eine einfache Webanwendung mit Vaadin " +
+                        "und Spring Boot zu erstellen."
+        );
+        description.setWidth("500px");
+        description.getStyle()
+                .set("font-size", "22px")
+                .set("line-height", "1.6")
+                .set("text-align", "left");
+
+        H3 name = new H3("TravelBooking GmbH");
+        H3 street = new H3("Spengergasse 20");
+        H3 city = new H3("1050 Wien");
+
+        add(companyName, subName, img, description, name, street, city);
     }
 
 }
--- src/main/java/com/example/application/views/travelbooking/TravelBookingView.java
+++ src/main/java/com/example/application/views/travelbooking/TravelBookingView.java
@@ -9,7 +9,9 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 import org.vaadin.lineawesome.LineAwesomeIconUrl;
-
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Paragraph;
 @PageTitle("TravelBooking")
 @Route("")
 @Menu(order = 1, icon = LineAwesomeIconUrl.FILE)
@@ -17,20 +19,44 @@ public class TravelBookingView extends VerticalLayout {
 
     public TravelBookingView() {
         setSpacing(false);
+        setAlignItems(Alignment.CENTER);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        H1 companyName = new H1("TravelBooking");
+        companyName.getStyle()
+                .set("font-family", "cursive")
+                .set("font-size", "6rem")
+                .set("margin", "0");
 
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        H2 subName = new H2("... your travel booking place ...");
+        subName.getStyle()
+                .set("margin", "0")
+                .set("color", "gray");
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        H2 title = new H2("Reiseangebote");
+
+        H2 zone1 = new H2("Wien - Italien");
+        Paragraph price1 = new Paragraph("Preis: 199,00 Euro");
+        Paragraph free1 = new Paragraph("Inklusive Hotel und Frühstück");
+
+        H2 zone2 = new H2("Wien - Paris");
+        Paragraph price2 = new Paragraph("Preis: 299,00 Euro");
+        Paragraph free2 = new Paragraph("Inklusive Flug und Hotel");
+
+        H2 zone3 = new H2("Wien - Barcelona");
+        Paragraph price3 = new Paragraph("Preis: 349,00 Euro");
+        Paragraph free3 = new Paragraph("Inklusive Flug, Hotel und Stadtführung");
+
+        H2 zone4 = new H2("Wien - Dubai");
+        Paragraph price4 = new Paragraph("Preis: 599,00 Euro");
+        Paragraph free4 = new Paragraph("Luxusreise mit Hotel und Transfer");
+
+        add(
+                companyName, subName, title,
+                zone1, price1, free1,
+                zone2, price2, free2,
+                zone3, price3, free3,
+                zone4, price4, free4
+        );
     }
 
 }
